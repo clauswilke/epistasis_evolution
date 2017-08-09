3fb7705bdaec23a36d81d64a23198508fe7f4b3b
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2017-08-09 13:05:33 -0500

    fixing script names

--- bit_string_sim/src/write_run_equilib_test.sh
+++ bit_string_sim/src/write_run_equilib_test.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-repl_num=10
-sel_coef_arr=(0.1 0.01 0.001)  ##s*N < 1
+sel_coef_arr=(0.1 0.01 0.001 0.0001)  ##s*N < 1
 mut_prob_arr=(0.0001 0.001 0.01 0.1) ##pick mutation prob such that N*mu = 0.1, 1, 10
 eff_pop=100
 num_class=100
-k_start=0
-#eps_arr=(0.5 0 -1)
+k_start_arr=(0 $num_class)
+eps_arr=(1 0.5 0 -0.5 -1)
 
-if [ -f src/run_simulation.sh ]; then
-	rm src/run_simulation.sh 
+if [ -f src/run_equilib_test.sh ]; then
+	rm src/run_equilib_test.sh 
 fi
 
-for eps in $(seq -1 0.2 1.01)
+for eps in ${eps_arr[*]}
 do
 	for sel_coef in ${sel_coef_arr[*]}
 	do 	
 		for mut_prob in ${mut_prob_arr[*]} 	 
 		do
-			for i in $(seq 1 $repl_num) 
+			for k in ${k_start_arr[*]} 
 			do
-				out_file=complexity_evolution/bit_string_sim/varying_eps_sim/eps${eps}_s${sel_coef}_m${mut_prob}_rep${i}.txt
-				echo python complexity_evolution/bit_string_sim/src/evolve.py -s $sel_coef -m $mut_prob -N $eff_pop -L $num_class -k_start $k_start -eps $eps -o $out_file >> src/run_simulation.sh 
+				out_file=complexity_evolution/bit_string_sim/equilib_test/eps${eps}_s${sel_coef}_m${mut_prob}_k_start${k}.txt
+				echo python complexity_evolution/bit_string_sim/src/evolve.py -s $sel_coef -m $mut_prob -N $eff_pop -L $num_class -k_start $k_start -eps $eps -o $out_file >> src/run_equilib_test.sh 
 			done
 		done
 	done
