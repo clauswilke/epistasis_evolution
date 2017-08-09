4ea355b844d78d95e835134fef26b87ac9fd0cb2
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2017-08-08 22:35:58 -0500

    Updating script to run simulations

--- bit_string_sim/src/write_run_simulation.sh
+++ bit_string_sim/src/write_run_simulation.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 repl_num=10
-sel_coef_arr=(0.1 0.01 0.001 0.0001)  ##s*N < 1
+sel_coef_arr=(0.1 0.01 0.001)  ##s*N < 1
 mut_prob_arr=(0.0001 0.001 0.01 0.1) ##pick mutation prob such that N*mu = 0.1, 1, 10
 eff_pop=100
 num_class=100
 k_start=0
-eps_arr=(-1 0 0.5)
+#eps_arr=(seq -1 0.2 1.01)
 
 if [ -f src/run_simulation.sh ]; then
 	rm src/run_simulation.sh 
 fi
 
-for eps in ${eps_arr[*]}
+for eps in $(seq -1 0.2 1.01)
 do
 	for sel_coef in ${sel_coef_arr[*]}
 	do 	
