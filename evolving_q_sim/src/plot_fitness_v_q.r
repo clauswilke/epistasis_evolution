1e17db7f7799af9595a5e5e365963f411050861c
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2019-01-24 16:14:01 -0600

    Adding a new script to plot predicted fitness vs epistasis, and adding comments for plot_fitness_v_N.r

--- evolving_q_sim/src/plot_fitness_v_q.r
+++ evolving_q_sim/src/plot_fitness_v_q.r
@@ -23,46 +23,40 @@ predict_mean_fitness <- function(Ne, L, s, eps)
   sum(f*p_k)
 }
 
-# this function calculates population fitness for different sizes of population.
-w_vs_Ne <- function(eps, L, s, Nestart, Nestop, Nesteps = 0.1)
+# this function calculates population fitness for different values of epistasis coefficient.
+w_vs_eps <- function(Ne, L, s, epsstart, epsstop, epssteps = 0.01)
 {
-  Ne <- seq(Nestart, Nestop, Nesteps)  
-  w_ave <- vapply(Ne, function(Ne){predict_mean_fitness(Ne, L, s, eps)}, 1)
+  eps <- seq(epsstart, epsstop, epssteps)  
+  w_ave <- vapply(eps, function(eps){predict_mean_fitness(Ne, L, s, eps)}, 1)
   data.frame(s, w_ave, Ne, L, eps)
 }
 
 # set parameters to be used in numerical derivations
-q <- 1
-eps <- 1-q
+N <- 10
 s <- 0.01
-an_f1 <- w_vs_Ne(eps, 100, s, 0, 1000) %>% mutate(q = q)
+an_f1 <- w_vs_eps(N, 100, s, -1, 1) %>% mutate(q = 1 - eps)
 
-q <- 1.5
-eps <- 1-q
+N <- 50
 s <- 0.01
-an_f2 <- w_vs_Ne(eps, 100, s, 0, 1000) %>% mutate(q = q)
+an_f2 <- w_vs_eps(N, 100, s, -1, 1) %>% mutate(q = 1 - eps)
 
-q <- 0.5
-eps <- 1-q
+N <- 100
 s <- 0.01
-an_f3 <- w_vs_Ne(eps, 100, s, 0, 1000) %>% mutate(q = q)
+an_f3 <- w_vs_eps(N, 100, s, -1, 1) %>% mutate(q = 1 - eps)
 
 # combine all output
 rbind(an_f1, an_f2, an_f3) -> an_f
 
 # plot numerically calculated fitness for different N and q
-p <- ggplot(an_f, aes(x = Ne, y = w_ave, color = factor(q))) + 
-  geom_line(size = 1, lineend = "round", linejoin = "round") +
-  scale_color_manual(name = "q", values = c("#d3d4d8", "#3fbac2", "#4d606e")) +
-  xlab('N') +
+p <- ggplot(an_f, aes(x = q,y = w_ave)) +
+  geom_line(aes(color = factor(Ne)), size = 1.2) +
+  scale_color_manual(values = c("#f67280", "#c06c84", "#6c5b7c")) +
+  scale_y_continuous(breaks = seq(0, 1, 0.2)) +
+  coord_cartesian(ylim = c(0, 1)) +
+  xlab('q') +
   ylab('Mean fitness') +
-  scale_x_log10(limits = c(1, 1000), breaks = c(1, 10, 100, 1000), labels = c("1", "10", "100", "1000")) +
-  scale_y_continuous(limits = c(0, 1)) +
-  theme(axis.title = element_text(size = 14),
-        axis.text = element_text(size = 12),
-        legend.text = element_text(size = 11),
-        legend.title = element_text(size = 12))
+  guides(col = guide_legend(title = "N",reverse = TRUE))
 
 # save the plot
-save_plot(paste0(root_dir, "/evolving_q_sim/plots/fitness_v_N_s", s, "_A.png"), 
+save_plot(paste0(root_dir, "/evolving_q_sim/plots/fitness_v_q_s", s, ".png"), 
           p)
